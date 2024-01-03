23607bcccbfb32d24b3b01981c56f15c72df5ab4
nhatlam214 <lamnhat0421@gmail.com>
2024-01-03 22:23:10 +0700

    init project

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">lamtn.22git@vku.udn.vn</a>
+         <a href="mailto:anasbhai@gmail.com">thaindd.22git@vku.udn.vn</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>7:00am to 00:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">470 Tran Dai Nghia <br> Ngu Hanh Son - Da Nang</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">0988-xxx-xxx</a>
+         <a href="tel:1112223333">0964-xxx-xxx</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>NL</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
